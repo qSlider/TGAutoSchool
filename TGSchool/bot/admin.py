b7a83507c4093448a6df0825a47ff60b0d2993df
qSlider <m.strelecc@gmail.com>
2024-11-08 13:49:36 +0200

    0.9

--- TGSchool/bot/admin.py
+++ TGSchool/bot/admin.py
@@ -3,4 +3,4 @@ from .models import Question, Answer , Registration
 
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(Registration)
+admin.site.register(Registration)56
